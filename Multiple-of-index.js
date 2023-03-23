4b4813844a7fdaa01f976b6fb5870579be7e8d06
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-03-23 15:31:10 -0700

    Create Multiple-of-index.js

---
+++ Multiple-of-index.js
@@ -0,0 +1,18 @@
+/*
+Return a new array consisting of elements 
+which are multiple of their own index in 
+input array (length > 1).
+*/
+
+
+// Solution
+
+function multipleOfIndex(arr) {
+  let arr1 = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] % i == 0) {
+      arr1.push(arr[i]);
+    }
+  }
+  return arr1;
+}
